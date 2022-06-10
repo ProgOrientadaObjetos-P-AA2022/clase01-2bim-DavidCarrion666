5fb001ebb84799e49b0c1d3eb33c995e4427f18e
DavidCarrion666 <decarrion6@utpl.edu.ec>
2022-06-10 11:50:42 -0500

    Trabajo en Clase

--- Ejemplos/src/paquete1/Empresa.java
+++ Ejemplos/src/paquete1/Empresa.java
@@ -10,23 +10,84 @@ package paquete1;
  * @author reroes
  */
 public class Empresa {
+
     private String nombre;
-    private Edificio [] edificios;
+    private Edificio[] edificios;
     private double costoBienesInmuebles;
+    private double costoBienesInmueblesAutos;
+    private Vehiculo[] autos;
     
-    public void establecerNombre(String m){
+    public void establecerNombre(String m) {
         nombre = m;
     }
-    
-    public void establecerEdificios(Edificio[] m){
+
+    public void establecerEdificios(Edificio[] m) {
         edificios = m;
     }
     
-    public String obtenerNombre(){
-        return nombre;
+    public void estableceVehiculos(Vehiculo[] m) {
+        autos = m;
+    }
+
+    public void establecerCostoBienesInmuebles() {
+        double suma = 0;
+        for (int i = 0; i < edificios.length; i++) {
+            suma = suma + edificios[i].obtenerCosto();
+
+        }
+        costoBienesInmuebles = suma;
+
     }
     
-    public Edificio[] obtenerEdificios(){
+     public void establecerCostoBienesInmueblesAutos() {
+        double suma = 0;
+        for (int i = 0; i < autos.length; i++) {
+            suma = suma + autos[i].obtenerValor();
+
+        }
+        costoBienesInmueblesAutos = suma;
+
+    }
+
+    public String obtenerNombre() {
+        return nombre;
+    }
+
+    public Edificio[] obtenerEdificios() {
         return edificios;
+    }
+    
+      public Vehiculo[] obtenerVehiculo() {
+        return autos;
+    }
+
+    public double obtenerCostoBienesInmuebles() {
+        return costoBienesInmuebles;
+    }
+    
+     public double obtenerCostoBienesInmueblesAutos() {
+        return costoBienesInmueblesAutos;
+    }
+
+    @Override
+    public String toString() {
+
+        String reporte = String.format("%s\nLista De Edificios\n",
+                obtenerNombre());
+
+        for (int i = 0; i < obtenerEdificios().length; i++) {
+            reporte = String.format("%s%d. %s(%.1f)\n", 
+                    reporte,
+                    i+1,
+                    obtenerEdificios()[i].obtenerNombre().toUpperCase(),
+                    obtenerEdificios()[i].obtenerCosto());
+        }
+        reporte = String.format("%sCosto De Los Bienes Inmuebles: %.2f\n",
+                reporte,
+                costoBienesInmuebles);
+        
+        
+        return reporte;
+
     }
 }
--- Ejemplos/src/paquete2/Ejecutor.java
+++ Ejemplos/src/paquete2/Ejecutor.java
@@ -5,19 +5,45 @@
  */
 package paquete2;
 
+import paquete1.Edificio;
+import paquete1.Empresa;
+import paquete1.Vehiculo;
+
+/*import paquete1.*;*/
 /**
  *
  * @author reroes
  */
 public class Ejecutor {
+
     public static void main(String[] args) {
+        Edificio edf1 = new Edificio("Edificio Central");
+        edf1.establecerCostos(10000);
+        Edificio edf2 = new Edificio("Edificio Sur");
+        edf2.establecerCostos(20000);
+        Edificio edf3 = new Edificio("Edificio Norte");
+        edf3.establecerCostos(30000);
+        Edificio edf4 = new Edificio("Edificio Secundario");
+        edf4.establecerCostos(20000);
+        Edificio[] edificios = {edf1, edf2, edf3, edf4};
         
-        // Crear una arreglo de 4 elementos de tipo Edificio
+        Vehiculo v1 = new Vehiculo("Auto", "LBB0011", 10000);
+        Vehiculo v2 = new Vehiculo("Camioneta", "LCB0011", 20000);
+        Vehiculo v3 = new Vehiculo("Auto", "LBD0012", 6000);
+        Vehiculo v4 = new Vehiculo("Camioneta", "LCC0011", 10000);
+        Vehiculo v5 = new Vehiculo("Camioneta", "LDC0011", 25000);
+        Vehiculo[] autos = {v1, v2, v3, v4, v5};
+
+ 
+        Empresa miempresa = new Empresa();
+        miempresa.establecerNombre("Empresa De Hojas");
+        miempresa.establecerEdificios(edificios);
+        miempresa.estableceVehiculos(autos);
+        miempresa.establecerCostoBienesInmueblesAutos();
+        miempresa.establecerCostoBienesInmuebles();
         
-        // Crear un objeto de tipo Empresa:
-        //  Nombre: Empresa de Hojas
-        //  Lista de edificio: edf1, edf2, edf3, edf4
+
         
-        // Imprimir el costo de los bienes inmuebles de la empresa
+        System.out.println(miempresa);
     }
 }
